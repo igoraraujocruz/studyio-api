74d2a1820ff42b70b27a2f03ac2ba36f402f9e04
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-09-30 12:56:01 -0300

    add eslint, prettier and editorconfig

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ const app = express();
 
 app.get('/', (request, response) => {
     return response.json({
-        message: 'hello world'
+        message: 'hello world',
     });
-})
+});
 
 app.listen(3333, () => {
-    console.log("server started on port 3333!")
-})
+    console.log('server started on port 3333!');
+});
