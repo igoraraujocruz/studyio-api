ed04382db14b0eb1f4ad9626a59c8dea26522b8e
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-09-30 20:52:58 -0300

    add BaseRepository and refactor User Service

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,20 +1,38 @@
 import { getRepository, Repository } from 'typeorm';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import BaseRepository from '@shared/infra/typeorm/repositories/BaseRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
 
-export default class UsersRepository implements IUsersRepository {
-    private ormRepository: Repository<User>;
+export default class UsersRepository extends BaseRepository<User> implements IUsersRepository {
+    readonly ormRepository: Repository<User>;
 
     constructor() {
-        this.ormRepository = getRepository(User);
+        const repo = getRepository(User);
+        super(repo);
+        this.ormRepository = repo;
     }
 
-    public async create(data: Partial<User>): Promise<User> {
-        const user = this.ormRepository.create(data);
-        return this.ormRepository.save(user);
+    public async findByUsername(username: string): Promise<User | undefined> {
+        const item = this.ormRepository.findOne({
+            where: { username },
+        });
+
+        return item;
     }
 
-    public async save(user: User): Promise<User> {
-        return this.ormRepository.save(user);
+    public async findByEmail(email: string): Promise<User | undefined> {
+        const item = this.ormRepository.findOne({
+            where: { email },
+        });
+
+        return item;
+    }
+
+    public async findByMobilePhone(mobilePhone: string): Promise<User | undefined> {
+        const item = this.ormRepository.findOne({
+            where: { mobilePhone },
+        });
+
+        return item;
     }
 }
--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,6 +1,8 @@
 import User from '@modules/users/infra/typeorm/entities/User';
+import { IBaseRepository } from '@shared/repositories/IBaseRepository';
 
-export default interface IUsersRepository {
-    create(data: Partial<User>): Promise<User>;
-    save(user: User): Promise<User>;
+export default interface IUsersRepository extends IBaseRepository<User> {
+    findByUsername(username: string): Promise<User | undefined>
+    findByEmail(email: string): Promise<User | undefined>
+    findByMobilePhone(mobilePhone: string): Promise<User | undefined>
 }
