b624c2f61e23a87311af5e874ff6c2bf0d606370
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-09-30 15:56:46 -0300

    first route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-
-app.get('/', (request, response) => {
-    return response.json({
-        message: 'hello world',
-    });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('server started on port 3333!');
