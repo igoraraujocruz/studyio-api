8995fc04a707c1daefb156a2c09743bd89a0ab61
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-11-12 17:30:02 -0300

    fix: fix ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,3 @@
-const dir = process.env.NODE_ENV === 'dev' ? 'src' : 'dist';
-const file = process.env.NODE_ENV === 'dev' ? 'ts' : 'js';
-
 module.exports = {
     type: "postgres",
     host: process.env.POSTGRESQL_HOST,
@@ -9,12 +6,12 @@ module.exports = {
     password: process.env.POSTGRESQL_PASSWORD,
     database: process.env.POSTGRESQL_DATABASE,
     entities: [
-        `./${dir}/modules/**/infra/typeorm/entities/*.${file}`
+        `./dist/modules/**/infra/typeorm/entities/*.js`
     ],
     migrations: [
-        `./${dir}/shared/infra/typeorm/migrations/*.${file}`
+        `./dist/shared/infra/typeorm/migrations/*.js`
     ],
     cli: {
-        "migrationsDir": `./${dir}/shared/infra/typeorm/migrations/`
+        "migrationsDir": `./dist/shared/infra/typeorm/migrations/`
     }
 }
