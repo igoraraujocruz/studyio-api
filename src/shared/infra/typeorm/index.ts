de181c5bfd0b53c3ecf77cd4b00f4f407cc3cd56
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-11-16 19:26:20 -0300

    fix: fix connect

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,6 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
 
-const host = process.env.NODE_ENV === 'prod' ? 'database' : 'localhost';
+const host = process.env.NODE_ENV === 'dev' ? 'database' : 'localhost';
 
 interface IOptions {
     host: string;
